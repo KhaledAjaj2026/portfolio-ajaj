1ae0d5f908dea17d02272abcdaedcd686136171d
Khaled Ajaj <khaledajaj2026@gmail.com>
2022-06-29 15:10:15 -0700

    fixed missing bg image and missing jquery icon.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,32 +13,32 @@ function Skills() {
                     </li>
                     <li>
                         <div id="jquery" className="skill-icon">
-                            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/jquery_logo_icon_167804.png" alt="" />
+                            <img src="https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png" alt="jquery logo" />
                         </div>
                     </li>
                     <li>
                         <div id="bootstrap" className="skill-icon">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" alt="" />
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" alt="bootstrap logo" />
                         </div>
                     </li>
                     <li>
                         <div id="javascript" className="skill-icon">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png?20120221235433" alt="" />
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png?20120221235433" alt="javascript logo" />
                         </div>
                     </li>
                     <li>
                         <div id="react" className="skill-icon">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png?20220125121207" alt="" />
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png?20220125121207" alt="reaact logo" />
                         </div>
                     </li>
                     <li>
                         <div id="vscode" className="skill-icon">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519" alt="" />
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1024px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519" alt="vs code logo" />
                         </div>
                     </li>
                     <li>
                         <div id="terminal" className="skill-icon">
-                            <img src="https://www.verticalsysadmin.com/images/logos/Terminalicon2.png" alt="" />
+                            <img src="https://www.verticalsysadmin.com/images/logos/Terminalicon2.png" alt="terminal logo" />
                         </div>
                     </li>
                 </ul>
